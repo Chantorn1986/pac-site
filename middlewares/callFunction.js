89b648309bd255c55160aa40040529b886ddee7c
Chantorn1986 <p.chantorn1986@gmail>
2025-10-06 16:37:20 +0700

    add some fretures

--- middlewares/callFunction.js
+++ middlewares/callFunction.js
@@ -20,5 +20,27 @@ const storageTypeProducts = multer.diskStorage({
   }
 })
 
+const storageEmployee = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/employee')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname)
+  }
+})
+
+const storageEmployeeCard = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads/employeeCard')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix + file.originalname)
+  }
+})
+
 exports.uploadBrands = multer({ storage: storageBrands }).single('file')
 exports.uploadTypeProducts = multer({ storage: storageTypeProducts }).single('file')
+exports.uploadEmployee = multer({ storage: storageEmployee }).single('file')
+exports.uploadEmployeeCard = multer({ storage: storageEmployeeCard }).single('file')
